b8035cc96e936af59c528f53d9ccfe5f34a09a56
dynkim <doyeangkim@gmail.com>
2022-06-02 21:10:18 +0900

    아무거나

--- Assets/Script/CubeScript.cs
+++ Assets/Script/CubeScript.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class CubeScript : MonoBehaviour
 {
-    public float moveSpeed = 10.0f;
-    public float turnSpeed = 80.0f;
+    public float moveSpeed = 30.0f;
+    public float turnSpeed = 100.0f;
     
     private float h;
     private float v;
